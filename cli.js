91b2cbebe713964ae625a4d84b3311c76f8fd0b3
rosi-souza <rosi.souza@live.com>
2019-12-02 14:15:43 -0300

    add lint

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-const mdLinks = require("./lib/index.js")
+const mdLinks = require("./lib/index.js");
 
 mdLinks(process.argv[2])
-	.then((result) => (console.log(result))
-  .catch((error) => (console.log(error))))
+  .then((result) => (console.log(result))
+    .catch((error) => (console.log(error))));
