6bd8904e3453662b1ba0c991c6685648dab7fe61
rosi-souza <rosi.souza@live.com>
2019-12-03 13:22:19 -0300

    add forech para retirar do objeto

--- cli.js
+++ cli.js
@@ -2,5 +2,5 @@
 const mdLinks = require("./lib/index.js");
 
 mdLinks(process.argv[2])
-  .then((result) => (console.log(result))
+  .then((result) => (result.forEach( x => console.log(x.href, x.text)))
     .catch((error) => (console.log(error))));
