52daeff87a1d853c237334d5f800c9506c06a12e
rosi-souza <rosi.souza@live.com>
2019-12-03 13:53:08 -0300

    add limit in text

--- cli.js
+++ cli.js
@@ -2,5 +2,5 @@
 const mdLinks = require("./lib/index.js");
 
 mdLinks(process.argv[2])
-  .then((result) => (result.forEach( x => console.log(x.href, x.text)))
-    .catch((error) => (console.log(error))));
+  .then((result) => result.forEach( x => console.log(x.href.substring(0, 50), x.text.substring(0, 50))))
+  .catch((error) => console.log(error));
