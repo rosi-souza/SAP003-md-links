fc0c14ea601fb61d17dd08eba8a005e55af20fba
rosi-souza <rosi.souza@live.com>
2019-12-04 08:01:06 -0300

    add readme

--- cli.js
+++ cli.js
@@ -2,5 +2,5 @@
 const mdLinks = require("./lib/index.js");
 
 mdLinks(process.argv[2])
-  .then((result) => result.forEach( x => console.log(x.href.substring(0, 50), x.text.substring(0, 50))))
+  .then((result) => result.forEach( element => console.log(element.href, element.text.substring(0, 50))))
   .catch((error) => console.log(error));
